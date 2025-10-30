02ddf85802991efd4ec9168f5d6986b0c6c29a6f
Muhammad Faisal <muh.faisal572@gmail.com>
2025-10-30 15:53:14 +0700

    feat : install all input from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button>Click Me</Button>
+      <Card className="max-w-md w-full">
+        <CardHeader>
+          <h2 className="font-bold text-green-500 text-center text-xl">
+            Search Your Product
+          </h2>
+        </CardHeader>
+        <CardContent>
+          <div className="grid grid-cols-2 gap-4">
+            <Button>Search</Button>
+            <Button>hi</Button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
