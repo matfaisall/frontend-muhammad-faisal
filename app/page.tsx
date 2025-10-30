441c0cd45003bb0ebbac495475d23274d784f9a3
Muhammad Faisal <muh.faisal572@gmail.com>
2025-10-30 15:39:06 +0700

    initiate : initiate project using next and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>helo world</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
